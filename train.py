b3be873384accbfc2d14f0586f336ae4959ec509
Curro <curro.ramosperez@gmail.com>
2022-06-03 16:54:07 +0200

    parsed arg

--- train.py
+++ train.py
@@ -37,144 +37,146 @@ window_size = 24 * 7 # 4 days
 
 epochs = 2
 
-# Load and prepare the dataset
-series,times = load_dataset(dataset)
+if __name__ =='__main__':
+        
+    # Load and prepare the dataset
+    series,times = load_dataset(dataset)
 
-# Plot some data
-plt.figure(figsize=(10, 6))
-plot_series(times, series, start=0, end = 24 * 7)
-plt.title('Data used sample')
-
-
-# Normalize the dataset
-scaler = MinMaxScaler(feature_range=(0.1, 1)) 
-series_scaled = scaler.fit_transform(series)
-
-# Split training and validation sets
-split_time = int(len(times) * 0.66)
-
-time_train = times[:split_time]
-x_train = series[:split_time] # Not scaled
-x_train_scaled = series_scaled[:split_time]
-
-time_test = times[split_time:]
-x_test = series[split_time:] # Not scaled
-x_test_scaled = series_scaled[split_time:]
-
-# Create the data generators for training
-num_features = 1
-batch_size=16
-train_generator = TimeseriesGenerator(x_train_scaled,
-                                      x_train_scaled,
-                                      length=window_size,
-                                      batch_size=batch_size)
-validation_generator = TimeseriesGenerator(x_test_scaled,
-                                           x_test_scaled,
-                                           length=window_size,
-                                           batch_size=batch_size)
-
-# Delete previous session to obtain static results
-tf.keras.backend.clear_session()
-tf.random.set_seed(51)
-np.random.seed(51)
-
-# Build the model:
-model = build_model(model_name,
-                   window_size,
-                   num_features)
-
-# Print model summary
-model.summary()
-
-# Configure callbacks
-callbacks = configure_callbacks(early_stopping = True,patience=10, 
-                                checkpoint= False, model_name= model_name )
-
-# Compilate the model  
-optimizer = 'adam'
-metrics = ['acc']
-model.compile(loss=tf.keras.losses.Huber(),
-              optimizer=optimizer,
-              metrics=metrics)
-
-# Train model
-history = model.fit_generator(generator=train_generator,
-                              verbose=1,
-                              epochs=epochs,
-                              validation_data=validation_generator,
-                              callbacks=callbacks)
-
-# Create new dir to save results
-try:
-    os.mkdir(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results')
-except:
-    pass
-
-# Save the trained model 
-model.save(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/{model_name}_ws{window_size}_epochs{epochs}_model.h5')
-
-# Plot the training and validation accuracy and loss at each epoch
-plot_training(history)
-plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/history.png')
+    # Plot some data
+    plt.figure(figsize=(10, 6))
+    plot_series(times, series, start=0, end = 24 * 7)
+    plt.title('Data used sample')
 
 
-# Run predictions on training and validation set
-train_predict = predict(model,
-                        train_generator,
+    # Normalize the dataset
+    scaler = MinMaxScaler(feature_range=(0.1, 1)) 
+    series_scaled = scaler.fit_transform(series)
+
+    # Split training and validation sets
+    split_time = int(len(times) * 0.66)
+
+    time_train = times[:split_time]
+    x_train = series[:split_time] # Not scaled
+    x_train_scaled = series_scaled[:split_time]
+
+    time_test = times[split_time:]
+    x_test = series[split_time:] # Not scaled
+    x_test_scaled = series_scaled[split_time:]
+
+    # Create the data generators for training
+    num_features = 1
+    batch_size=16
+    train_generator = TimeseriesGenerator(x_train_scaled,
+                                        x_train_scaled,
+                                        length=window_size,
+                                        batch_size=batch_size)
+    validation_generator = TimeseriesGenerator(x_test_scaled,
+                                            x_test_scaled,
+                                            length=window_size,
+                                            batch_size=batch_size)
+
+    # Delete previous session to obtain static results
+    tf.keras.backend.clear_session()
+    tf.random.set_seed(51)
+    np.random.seed(51)
+
+    # Build the model:
+    model = build_model(model_name,
+                    window_size,
+                    num_features)
+
+    # Print model summary
+    model.summary()
+
+    # Configure callbacks
+    callbacks = configure_callbacks(early_stopping = True,patience=10, 
+                                    checkpoint= False, model_name= model_name )
+
+    # Compilate the model  
+    optimizer = 'adam'
+    metrics = ['acc']
+    model.compile(loss=tf.keras.losses.Huber(),
+                optimizer=optimizer,
+                metrics=metrics)
+
+    # Train model
+    history = model.fit_generator(generator=train_generator,
+                                verbose=1,
+                                epochs=epochs,
+                                validation_data=validation_generator,
+                                callbacks=callbacks)
+
+    # Create new dir to save results
+    try:
+        os.mkdir(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results')
+    except:
+        pass
+
+    # Save the trained model 
+    model.save(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/{model_name}_ws{window_size}_epochs{epochs}_model.h5')
+
+    # Plot the training and validation accuracy and loss at each epoch
+    plot_training(history)
+    plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/history.png')
+
+
+    # Run predictions on training and validation set
+    train_predict = predict(model,
+                            train_generator,
+                            scaler)
+
+    test_predict = predict(model,
+                        validation_generator,
                         scaler)
 
-test_predict = predict(model,
-                       validation_generator,
-                       scaler)
+    # Validate the model using RMSE and MAE
+    trainScore = math.sqrt(mean_squared_error(x_train[window_size:], train_predict[:,0]))
+    testScore = math.sqrt(mean_squared_error(x_test[window_size:], test_predict[:,0]))
+    print('Train Score: %.2f RMSE' % (trainScore))
+    print('Test Score: %.2f RMSE' % (testScore))
 
-# Validate the model using RMSE and MAE
-trainScore = math.sqrt(mean_squared_error(x_train[window_size:], train_predict[:,0]))
-testScore = math.sqrt(mean_squared_error(x_test[window_size:], test_predict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-print('Test Score: %.2f RMSE' % (testScore))
+    train_mae = mean_absolute_error(x_train[window_size:], train_predict[:,0]) 
+    test_mae = mean_absolute_error(x_test[window_size:], test_predict[:,0])
+    print('Train Score: %.2f MAE' % (train_mae))
+    print('Test Score: %.2f MAE' % (test_mae))
 
-train_mae = mean_absolute_error(x_train[window_size:], train_predict[:,0]) 
-test_mae = mean_absolute_error(x_test[window_size:], test_predict[:,0])
-print('Train Score: %.2f MAE' % (train_mae))
-print('Test Score: %.2f MAE' % (test_mae))
+    # Write validation results on csv
+    csv_file = open(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/validation_metrics.txt', 'w')
+    csv_writer = csv.writer(csv_file, delimiter = '\n')
+    csv_writer.writerow(['Train Score: %.2f RMSE' % (trainScore), 'Test Score: %.2f RMSE' % (testScore)])
+    csv_writer.writerow(['Train Score: %.2f MAE' % (train_mae), 'Test Score: %.2f MAE' % (test_mae)])
+    csv_file.close()
 
-# Write validation results on csv
-csv_file = open(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/validation_metrics.txt', 'w')
-csv_writer = csv.writer(csv_file, delimiter = '\n')
-csv_writer.writerow(['Train Score: %.2f RMSE' % (trainScore), 'Test Score: %.2f RMSE' % (testScore)])
-csv_writer.writerow(['Train Score: %.2f MAE' % (train_mae), 'Test Score: %.2f MAE' % (test_mae)])
-csv_file.close()
+    # Plot train and test predictions
+    plot_predictions(train_predict,
+                    test_predict,
+                    window_size,
+                    times,
+                    series,
+                    x_train)
 
-# Plot train and test predictions
-plot_predictions(train_predict,
-                  test_predict,
-                  window_size,
-                  times,
-                  series,
-                  x_train)
+    # Plot train and test predictions separately
+    plt.figure()
+    plt.plot(x_train[window_size:])
+    plt.plot(train_predict[:,0])
+    plt.title('Predictions on train')
+    plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_train.png')
 
-# Plot train and test predictions separately
-plt.figure()
-plt.plot(x_train[window_size:])
-plt.plot(train_predict[:,0])
-plt.title('Predictions on train')
-plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_train.png')
-
-plt.figure()
-plt.plot(x_test[window_size:])
-plt.plot(test_predict[:,0])
-plt.title('Predictions on test')
-plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_test.png')
+    plt.figure()
+    plt.plot(x_test[window_size:])
+    plt.plot(test_predict[:,0])
+    plt.title('Predictions on test')
+    plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_test.png')
 
 
-# Plot train and test with zoom
-plot_gtruth_and_predictions(x_train, train_predict, window_size, start = 0, end = 168)
-plt.title('Predictions on train (zoomed sample)')
-plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_train_zoom.png')
+    # Plot train and test with zoom
+    plot_gtruth_and_predictions(x_train, train_predict, window_size, start = 0, end = 168)
+    plt.title('Predictions on train (zoomed sample)')
+    plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_train_zoom.png')
 
-plot_gtruth_and_predictions(x_test, test_predict, window_size, start = 0, end = 168)
-plt.title('Predictions on test (zoomed sample)')
-plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_test_zoom.png')
+    plot_gtruth_and_predictions(x_test, test_predict, window_size, start = 0, end = 168)
+    plt.title('Predictions on test (zoomed sample)')
+    plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/pred_test_zoom.png')
 
 
 
@@ -189,7 +191,7 @@ plt.savefig(f'Results/train_{model_name}_ws{window_size}_epochs{epochs}_results/
 
 
 
-    
+        
 
 
 
